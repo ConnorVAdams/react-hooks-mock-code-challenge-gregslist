97382c506f74f2ba8015576de99024072897ae63
Connor Adams <connorvadams@gmail.com>
2023-10-08 18:08:39 -0600

    Adds persistent favoriting functionality.

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,14 +1,30 @@
-import React from "react";
+import { useState } from "react";
 
 function ListingCard({ id, description, image, location }) {
+  const [isFavorited, setIsFavorited] = useState(false)
+  
+  const handleFavorite = (e) => {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        favorited: !isFavorited
+      })
+    })
+    .then(resp => resp.json())
+    .then(setIsFavorited(isFavorited => !isFavorited))
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
         <img src={image} alt={description} />
       </div>
-      <div className="details">
-        {true ? (
+      <div className="details" onClick={handleFavorite}>
+        {isFavorited ? (
           <button className="emoji-button favorite active">★</button>
         ) : (
           <button className="emoji-button favorite">☆</button>
